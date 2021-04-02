4a7d06a87bbe4a5fba77b8cf6da4ca25d08f74ce
VincVader <vincvader@mail.ru>
2021-04-02 05:16:35 +0300

    added some comments for better understanding functions

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,7 +13,8 @@ const rng = (min, max) => {
 };
 /**
  * getQuote  - gets random quote
- * @return {object} - with cite and author
+ * @return {object} - with keys cite and author filled with
+ * given values from api
  */
 async function getQuote() {
     const response = await fetch('https://quotes15.p.rapidapi.com/quotes/random/', {
@@ -38,7 +39,7 @@ async function getQuote() {
 /**
  * Bruh
  * @param {string} text - text which should be checked for bad words
- * @return {promise}
+ * @return {promise} - on resolve return true/false
  */
 async function filterBadWords(text) {
     const test = await fetch(`https://community-purgomalum.p.rapidapi.com/containsprofanity?text=${encodeURI(text)}`, {
@@ -58,23 +59,35 @@ async function filterBadWords(text) {
 }
 let quote;
 
-const colorize = () => {
+
+/**
+ * gets random values (in fixed range)
+ * @return {string} - hsl(hue, saturation%, lightness%) - nice "random" color
+ */
+function colors() {
+    const h = rng(0, 360);
+    const s = rng(42, 98);
+    const l = rng(40, 70);
+    return `hsl(${h},${s}%,${l}%)`;
+}
+
+/**
+ * gets hsl valus from  ^ colors funciton and
+ * sets background and text color to that value
+ * for elements on page
+ */
+function colorize() {
     const color = colors();
     quoteWrapper.style.background = color;
     quoteText.style.color = color;
     quoteAuthor.style.color = color;
     quoteButton.style.background = color;
-};
-
-const colors = () => {
-    const h = rng(0, 360);
-    const s = rng(42, 98);
-    const l = rng(40, 70);
-    return `hsl(${h},${s}%,${l}%)`;
-};
+}
 
 /**
- * check
+ * checks quote for containig bad words
+ * if so => replaces quote with default one
+ * also disables button for ~ 1 sec that user cannot spam api-service
  */
 async function check() {
     colorize();
@@ -93,7 +106,5 @@ async function check() {
     }, 1000);
 }
 
-colorize();
-quoteButton.addEventListener('click', check);
-
-
+colorize(); // colorize page with some nice color
+quoteButton.addEventListener('click', check); // generates random cite
